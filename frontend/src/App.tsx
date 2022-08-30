af5f7051c74c2131bfab4060f89aeff8e2a04709
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-08-31 00:09:11 +0530

    Added mantine-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  MantineProvider,
+  Title,
+  MantineThemeOverride,
+  Tuple,
+} from "@mantine/core";
+import { colors } from "./colors";
+import PictureDrop from "./components/PictureDrop";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type CustomColors = keyof typeof colors;
+declare module "@mantine/core" {
+  export interface MantineThemeColorsOverride {
+    colors: Record<CustomColors, Tuple<string, 10>>;
+  }
 }
 
-export default App;
+const mantineColors = Object.fromEntries(
+  Object.entries(colors)
+    .filter(([k, v]) => typeof v !== "string")
+    .map(([k, v]) => [k, Object.values(v)])
+);
+
+const themeConfig: MantineThemeOverride = {
+  colors: mantineColors,
+  colorScheme: 'dark',
+};
+
+export default function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={themeConfig}>
+      <div className="app-main bg-zinc-900 h-screen w-screen flex flex-col text-zinc-200 font-roboto overflow-hidden">
+        <div className="app-header text-center mt-[50px]">
+          <h2 className="text-2xl font-medium">Bengaluru City Police</h2>
+          <h1 className="text-3xl font-semibold">
+            Facial Recognition System Demo
+          </h1>
+        </div>
+        <div className="dropzone-container mt-[30px] px-[300px] py-[px]">
+          <PictureDrop />
+        </div>
+      </div>
+    </MantineProvider>
+  );
+}
