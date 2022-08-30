a7c76623e9e3c3cc962db475c6ca7e669df9e18d
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-08-31 01:54:16 +0530

    Changed PictureDrop to fileinput

--- frontend/src/components/PictureDrop.tsx
+++ frontend/src/components/PictureDrop.tsx
@@ -1,104 +1,40 @@
-import { useRef } from "react";
-import {
-  Button,
-  Group,
-  useMantineTheme,
-  Text,
-  Image,
-  ScrollArea,
-} from "@mantine/core";
-import { Dropzone, FileWithPath } from "@mantine/dropzone";
-import { IconUpload, IconPhoto, IconX } from "@tabler/icons";
+import React, { SyntheticEvent, useRef } from "react";
+import { useMantineTheme, Image, FileInput } from "@mantine/core";
+import { IconUpload } from "@tabler/icons";
 import { useState } from "react";
 import AnalysisArea from "./AnalysisArea";
 
+interface ImageData {
+  file?: File;
+  blob: string;
+  filename: string;
+}
+
 export default function PictureDrop() {
-  const openRef = useRef<() => void>(null);
-  const theme = useMantineTheme();
-  const [files, setFiles] = useState<FileWithPath[]>([]);
-
-  const previews = files.map((file, index) => {
-    const imageUrl = URL.createObjectURL(file);
-    return (
-      <Image
-        key={index}
-        width={250}
-        height={250}
-        src={imageUrl}
-        radius="sm"
-        imageProps={{ onLoad: () => URL.revokeObjectURL(imageUrl) }}
-      />
-    );
+  const [img, setImg] = useState<ImageData>({
+    file: undefined,
+    blob: "",
+    filename: "",
   });
-
+  const handleChange = (file: File | null) => {
+    file !== null
+      ? setImg((prev: ImageData) => ({
+          file: file,
+          blob: URL.createObjectURL(file),
+          filename: file.name,
+        }))
+      : console.log("err");
+  };
   return (
     <>
-      <Dropzone
-        onDrop={setFiles}
-        onReject={(files) => console.log("rejected files", files)}
-        maxSize={3 * 1024 ** 2}
-        accept={{
-          "image/*": [".jpg", ".png", ".jpeg"],
-        }}
-        openRef={openRef}
-        sx={(theme) => ({
-          backgroundColor: theme.colors.zinc[8],
-          border: `dashed 1px ${theme.colors.zinc[4]}`,
-          "&:hover": {
-            backgroundColor: theme.colors.zinc[7],
-          },
-        })}
-      >
-        <Group
-          position="center"
-          spacing="sm"
-          style={{ minHeight: 110, pointerEvents: "none" }}
-        >
-          <Dropzone.Accept>
-            <IconUpload
-              size={50}
-              stroke={1.5}
-              color={
-                theme.colors[theme.primaryColor][
-                  theme.colorScheme === "dark" ? 4 : 6
-                ]
-              }
-            />
-          </Dropzone.Accept>
-          <Dropzone.Reject>
-            <IconX
-              size={50}
-              stroke={1.5}
-              color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
-            />
-          </Dropzone.Reject>
-          <Dropzone.Idle>
-            <IconPhoto size={50} stroke={1.5} />
-          </Dropzone.Idle>
-
-          <div>
-            <Text size="xl" inline>
-              Drag image here or click to select file
-            </Text>
-            <Text size="sm" color="dimmed" inline mt={7}>
-              File should not exceed 5mb
-            </Text>
-          </div>
-        </Group>
-      </Dropzone>
-      <Group position="center" mt="md">
-        <button
-          onClick={() =>
-            openRef.current !== null
-              ? openRef.current()
-              : console.log("Ref null")
-          }
-          className="px-8 py-3 rounded bg-black font-semibold text-zinc-200 font-roboto hover:bg-zinc-800"
-        >
-          Select files
-        </button>
-      </Group>
-      <AnalysisArea previews={previews}/>
+      <form>
+        <FileInput
+          placeholder="Upload image"
+          icon={<IconUpload size={14} />}
+          accept="image/png, image/jpeg"
+          onChange={(file) => handleChange(file)}
+        />
+      </form>
     </>
   );
 }
