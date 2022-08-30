68905b6c62e8772d4c08c7af61c874e17474c0fa
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-08-31 01:55:04 +0530

    Added initial api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['POST'])
+def post_image(request, *args, **kwargs):
+    print(request.data)
+    return Response({'data': 'hello world!'})
