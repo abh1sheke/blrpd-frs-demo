7ff9e79c0d15845db9e4de4e1dab6875e94a60d6
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2022-08-31 16:20:38 +0530

    Added http requests for uploading images

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', post_image)
+    path('', post_image),
+    path('post_image/', SourceImageView.as_view(), name='post_image')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,29 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+
+from images.models import SourceImage
+from images.serializers import SourceImageSerializer
+
+
+class SourceImageView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        image_serializer = SourceImageSerializer(data=request.data)
+        if image_serializer.is_valid():
+            image_serializer.save()
+            return Response(image_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', image_serializer.errors)
+            return Response(image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.
-@api_view(['POST'])
+
+
+@api_view(['GET'])
 def post_image(request, *args, **kwargs):
-    print(request.data)
-    return Response({'data': 'hello world!'})
+    return Response({'message': 'You are viewing API Home'})
