83f736506e7a8b4c865d80c32b135bb40dd0cc47
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2022-08-31 16:26:37 +0530

    Pictures can now be uploaded to server

--- frontend/src/components/PictureDrop.tsx
+++ frontend/src/components/PictureDrop.tsx
@@ -1,39 +1,81 @@
-import React, { SyntheticEvent, useRef } from "react";
-import { useMantineTheme, Image, FileInput } from "@mantine/core";
+import { FileInput } from "@mantine/core";
 import { IconUpload } from "@tabler/icons";
+import { FormEvent } from "react";
+import { selectImageState, setImageData } from "../store/sessionSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { ImageData } from "../store/sessionSlice";
 import { useState } from "react";
-import AnalysisArea from "./AnalysisArea";
+import axios from "axios";
 
-interface ImageData {
-  file?: File;
-  blob: string;
-  filename: string;
+function FormButton({ imageData }: { imageData?: ImageData }) {
+  const base = "mx-auto px-6 py-2 font-roboto rounded transition-all";
+  const enabled =
+    "text-zinc-300 bg-black hover:bg-zinc-700 hover:ring-1 hover:ring-zinc-800 cursor-pointer";
+  const disabled = "text-zinc-300 bg-black opacity-40";
+  return (
+    <>
+      {imageData ? (
+        <>
+          <button className={`${base} ${enabled}`} type="submit">
+            Find match
+          </button>
+        </>
+      ) : (
+        <>
+          <button className={`${base} ${disabled}`} disabled type="submit">
+            Find match
+          </button>
+        </>
+      )}
+    </>
+  );
 }
 
 export default function PictureDrop() {
-  const [img, setImg] = useState<ImageData>({
-    file: undefined,
-    blob: "",
-    filename: "",
-  });
+  const [img, setImg] = useState<Blob | string>('');
+  const imageData = useSelector(selectImageState);
+  const dispatch = useDispatch();
   const handleChange = (file: File | null) => {
+    file !== null ? setImg(file as Blob) : console.log("err");
     file !== null
-      ? setImg((prev: ImageData) => ({
-          file: file,
-          blob: URL.createObjectURL(file),
-          filename: file.name,
-        }))
+      ? dispatch(
+          setImageData({
+            ...imageData,
+            blob: URL.createObjectURL(file),
+            filename: file.name,
+          })
+        )
       : console.log("err");
+  };
+  const handleSubmit = async (e: FormEvent) => {
+    console.log("submitted");
+    e.preventDefault();
+    let formData = new FormData();
+    console.log(img);
+    formData.append("image", img);
+    formData.append("filename", imageData?.filename as string);
+    axios
+      .post("http://localhost:8000/api/post_image/", formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <>
-      <form>
+      <form className="flex flex-col" onSubmit={(e) => handleSubmit(e)}>
         <FileInput
+          className="my-5"
           placeholder="Upload image"
           icon={<IconUpload size={14} />}
           accept="image/png, image/jpeg"
           onChange={(file) => handleChange(file)}
         />
+        <FormButton imageData={imageData} />
       </form>
     </>
   );
