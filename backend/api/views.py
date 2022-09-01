2d65e36910a81cde789f03f407809ab7f278ce99
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2022-09-01 15:26:53 +0530

    Added face recogintion function

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,25 +3,47 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
+from django.conf import settings
 
 from images.models import SourceImage
 from images.serializers import SourceImageSerializer
+from .rekognition import FaceRecognition
 
 
 class SourceImageView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
+    def get_faces(self, filename, image_path, detect_bucket, search_bucket, tablename):
+        print(settings.MEDIA_ROOT)
+        face_rek = FaceRecognition(
+            filename, image_path, detect_bucket, search_bucket, tablename, settings.MEDIA_ROOT)
+        images = face_rek.detectFaces()
+        matches = []
+        key = 0
+        for image in images:
+            match = face_rek.findMatches(image, key)
+            if match != None:
+                matches.append(match)
+                key += 1
+        return matches
+
     def post(self, request, *args, **kwargs):
-        print(request.data)
         image_serializer = SourceImageSerializer(data=request.data)
         if image_serializer.is_valid():
             image_serializer.save()
-            return Response(image_serializer.data, status=status.HTTP_201_CREATED)
+            filename = image_serializer.data['image'].split('/')[3]
+            image_path = f'{settings.MEDIA_ROOT}/images/{filename}'
+            matches = self.get_faces(
+                filename=filename,
+                image_path=image_path,
+                detect_bucket='detect-face',
+                search_bucket='blrpd-frs-demo',
+                tablename='blrpd-frs-demo'
+            )
+            return Response({'data': image_serializer.data, 'matches': matches}, status=status.HTTP_201_CREATED)
         else:
             print('error', image_serializer.errors)
             return Response(image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# Create your views here.
 
 
 @api_view(['GET'])
