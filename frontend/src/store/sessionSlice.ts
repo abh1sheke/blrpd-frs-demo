aa31e86780934b58884a3a987cdd59aa1350fd83
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2022-09-01 15:29:40 +0530

    Added returnData state variable and MatchData interface

--- frontend/src/store/sessionSlice.ts
+++ frontend/src/store/sessionSlice.ts
@@ -1,18 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { AppState } from "./store";
 
+interface MatchData {
+  FaceId: {
+    Item: {
+      RekognitionId: {
+        S: string;
+      };
+      FullName: {
+        S: string;
+      };
+      Filename: {
+        S: string;
+      };
+    };
+  };
+  Match: {
+    Similarity: number;
+  };
+  MatchFile: string;
+}
 export interface ImageData {
-  file?: File;
+  returnData?: {
+    data: {
+      id: number;
+      image: string;
+    };
+    matches: MatchData[];
+  };
   blob?: string;
   filename?: string;
 }
 
 export interface SessionState {
-  imageData?: ImageData;
+  imageData: ImageData;
 }
 
 const initialState: SessionState = {
-  imageData: undefined,
+  imageData: {
+    returnData: undefined,
+    blob: undefined,
+    filename: undefined,
+  },
 };
 
 export const sessionSlice = createSlice({
@@ -22,10 +51,13 @@ export const sessionSlice = createSlice({
     setImageData(state, action) {
       state.imageData = action.payload;
     },
+    setReturnData(state, action) {
+      state.imageData.returnData = action.payload;
+    },
   },
 });
 
-export const { setImageData } = sessionSlice.actions;
+export const { setImageData, setReturnData } = sessionSlice.actions;
 export const selectImageState = (state: AppState) => state.session.imageData;
 
 export default sessionSlice.reducer;
